54fbcd889679dad8888014a64c2abbfca57a0c43
trethore <titou.rethore@gmail.com>
2026-03-21 16:54:12 +0100

    feat: update Docker and build scripts for improved Windows ARM64 support

--- compile_windows.bat
+++ compile_windows.bat
@@ -1,5 +1,7 @@
 @echo off
 
+setlocal
+
 if ("%2"=="") ( ^ 
     echo "Usage: compile_windows.bat <architecture> <buildType> [<gitrepo> <gitref>]" && ^ 
     echo "" && ^ 
@@ -20,13 +22,18 @@ else (set "REF=%4")
 
 :: Execute build with windows Dockerfile
 docker build -t jcefbuild --file DockerfileWindows .
+if errorlevel 1 exit /b %errorlevel%
 
 :: Execute run with windows Dockerfile
 if not exist "jcef" mkdir "jcef"
-rmdir /S /Q out
+rmdir /S /Q out 2>nul
 mkdir "out"
+docker rm -f jcefbuild >nul 2>&1
 docker run --name jcefbuild -v jcef:"C:\jcef" -e TARGETARCH=%1 -e BUILD_TYPE=%2 -e REPO=%REPO% -e REF=%REF% jcefbuild
+if errorlevel 1 exit /b %errorlevel%
 docker cp jcefbuild:C:\out\binary_distrib.tar.gz out\binary_distrib.tar.gz
+if errorlevel 1 exit /b %errorlevel%
+docker rm jcefbuild >nul 2>&1
 if not exist "out\binary_distrib.tar.gz" (
     echo ERROR: out\binary_distrib.tar.gz not found after build.
     exit /b 1
--- scripts/run_windows.bat
+++ scripts/run_windows.bat
@@ -1,51 +1,95 @@
 @echo off
 
+setlocal
+
+set "VCVARS_BAT="
+set "JAVA_HOME_DIR="
+set "DISTRIB_DIR="
+
 if "%TARGETARCH%"=="386" (echo "Building 32-bit version") ^
 else (echo "Building 64-bit version")
 
+if "%TARGETARCH%"=="386" (
+    set "VCVARS_BAT=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
+    set "JAVA_HOME_DIR=C:/Program Files (x86)/Java/jdk1.8.0_211"
+    set "DISTRIB_DIR=win32"
+)
+if "%TARGETARCH%"=="amd64" (
+    set "VCVARS_BAT=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+    set "JAVA_HOME_DIR=C:/Program Files/Java/jdk1.8.0_211"
+    set "DISTRIB_DIR=win64"
+)
+if "%TARGETARCH%"=="arm64" (
+    set "VCVARS_BAT=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsamd64_arm64.bat"
+    set "JAVA_HOME_DIR=C:/jdk-11"
+    set "DISTRIB_DIR=win64"
+)
+
+if not defined VCVARS_BAT (
+    echo ERROR: Unsupported TARGETARCH "%TARGETARCH%"
+    exit /b 1
+)
+
+if not exist "%VCVARS_BAT%" (
+    echo ERROR: Required Visual Studio environment script not found: %VCVARS_BAT%
+    exit /b 1
+)
+
 :: Update ssl certs
 certutil -generateSSTFromWU roots.sst && certutil -addstore -f root roots.sst && del roots.sst
+if errorlevel 1 exit /b %errorlevel%
 
 :: Check residency of workdir
 cd ..
+if errorlevel 1 exit /b %errorlevel%
 if exist "jcef\README.md" (echo "Found existing files to build" && cd jcef) ^
 else (echo "Did not find files to build - cloning..." && GOTO :CLONE)
+if errorlevel 1 exit /b %errorlevel%
 
 :BUILD
 :: CMakeLists patching 
 python C:/patch_cmake.py CMakeLists.txt C:/CMakeLists.txt.patch
+if errorlevel 1 exit /b %errorlevel%
 
 :: Prepare build dir
-mkdir jcef_build && cd jcef_build
+if not exist jcef_build mkdir jcef_build
+if errorlevel 1 exit /b %errorlevel%
+cd jcef_build
+if errorlevel 1 exit /b %errorlevel%
 
-:: Load vcvars for 32 or 64-bit builds
-if "%TARGETARCH%"=="386" (call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat")
-if "%TARGETARCH%"=="amd64" (call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat")
-if "%TARGETARCH%"=="arm64" (call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsamd64_arm64.bat")
+:: Load vcvars for the selected build
+call "%VCVARS_BAT%"
+if errorlevel 1 exit /b %errorlevel%
 
 :: Edit PATH variable on 386 to use 32 bit jdk (cmake findjni does not actually care about JAVA_HOME)
 if "%TARGETARCH%"=="386" (set "PATH=C:/Program Files (x86)/Java/jdk1.8.0_211;%PATH%")
 if "%TARGETARCH%"=="arm64" (set "PATH=C:/jdk-11;%PATH%")
 
 :: Perform build
-if "%TARGETARCH%"=="386" (cmake -G "Ninja" -DJAVA_HOME="C:/Program Files (x86)/Java/jdk1.8.0_211" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..)
-if "%TARGETARCH%"=="amd64" (cmake -G "Ninja" -DJAVA_HOME="C:/Program Files/Java/jdk1.8.0_211" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..)
-if "%TARGETARCH%"=="arm64" (cmake -G "Ninja" -DJAVA_HOME="C:/jdk-11" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..)
+cmake -G "Ninja" -DJAVA_HOME="%JAVA_HOME_DIR%" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
+if errorlevel 1 exit /b %errorlevel%
 ninja -j4
+if errorlevel 1 exit /b %errorlevel%
 
 :: Compile java classes
 cd ../tools
-if "%TARGETARCH%"=="386" (call compile.bat win32) else (call compile.bat win64)
+if errorlevel 1 exit /b %errorlevel%
+call compile.bat %DISTRIB_DIR%
+if errorlevel 1 exit /b %errorlevel%
 
 :: Create distribution
-if "%TARGETARCH%"=="386" (call make_distrib.bat win32) else (call make_distrib.bat win64)
+call make_distrib.bat %DISTRIB_DIR%
+if errorlevel 1 exit /b %errorlevel%
 
 :: Go to results
-if "%TARGETARCH%"=="386" (cd ../binary_distrib/win32) else (cd ../binary_distrib/win64)
+cd ../binary_distrib/%DISTRIB_DIR%
+if errorlevel 1 exit /b %errorlevel%
 :: Zip results to C:\out
-del /F C:\out\binary_distrib.tar.gz
+del /F C:\out\binary_distrib.tar.gz 2>nul
 if not exist "C:\out" mkdir "C:\out"
+if errorlevel 1 exit /b %errorlevel%
 tar -czvf C:\out\binary_distrib.tar.gz *
+if errorlevel 1 exit /b %errorlevel%
 
 GOTO :EOF
 
@@ -53,7 +97,11 @@ GOTO :EOF
 
 :CLONE
 if exist jcef rmdir /S /Q jcef
+if errorlevel 1 exit /b %errorlevel%
 git clone %REPO% jcef
+if errorlevel 1 exit /b %errorlevel%
 cd jcef
+if errorlevel 1 exit /b %errorlevel%
 git checkout %REF%
+if errorlevel 1 exit /b %errorlevel%
 GOTO :BUILD
